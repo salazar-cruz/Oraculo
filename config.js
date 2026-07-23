ab4fa1bed4f0da63630163046270f9d0d1677d0d
salazar-cruz <eConsulting.CV@gmail.com>
2026-07-23 16:25:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ORACULO_CONFIG = {
   // Substituir pelo endereço criado após publicar o Cloudflare Worker.
   apiUrl: "https://oraculo-da-palma-api.econsulting-cv.workers.dev",
-  debug: true
+  debug: false
 };
